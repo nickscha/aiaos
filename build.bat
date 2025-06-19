4e405844d4692f322006a01d0e19996c9898d021
nickscha <nick@scharnbeck.de>
2025-06-19 18:28:58 +0200

    [BUILD] add -march=x86-64

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo OFF
 
 REM CFLAGS with strict warnings and pedantic mode
-set DEF_COMPILER_FLAGS=-O2 -s -m64 -mno-red-zone -mno-stack-arg-probe -pedantic -std=c89 -nodefaultlibs -nostdlib -nostdinc ^
+set DEF_COMPILER_FLAGS=-O2 -s -m64 -mno-red-zone -mno-stack-arg-probe -march=x86-64 -pedantic -std=c89 -nodefaultlibs -nostdlib -nostdinc ^
 -ffreestanding -fno-builtin -fno-exceptions -fno-asynchronous-unwind-tables ^
 -Wall -Wextra -Werror -Wvla -Wconversion -Wdouble-promotion -Wsign-conversion -Wuninitialized -Winit-self ^
 -Wunused -Wunused-function -Wunused-macros -Wunused-parameter -Wunused-value -Wunused-variable -Wunused-local-typedefs
@@ -16,4 +16,4 @@ x86_64-elf-ld -T aiaos_linker.ld -o aiaos_linker.o aiaos_bootloader_stage1.s.o a
 x86_64-elf-objcopy -O binary aiaos_linker.o aiaos.img
 
 REM Run QEMU
-qemu-system-x86_64 -m 6g -no-reboot -drive file=aiaos.img,format=raw,index=0,media=disk
+qemu-system-x86_64 -m 1g -no-reboot -drive file=aiaos.img,format=raw,index=0,media=disk
