2299da71bda572c9074cb5d657439bf3e5aaec93
nickscha <nick@scharnbeck.de>
2025-07-01 20:09:44 +0200

    [BOOT] remove print string and format

--- aiaos_bootloader_stage1.asm
+++ aiaos_bootloader_stage1.asm
@@ -1,17 +1,13 @@
+section .boot_sector
+global __start
 
-    section .boot_sector
-    global __start
-
-    [bits 16]
+[bits 16]
 
 __start:
-    mov bx, hello_msg
-    call print_string
-
     mov si, disk_address_packet
     mov ah, 0x42 ; BIOS "extended read" function
     mov dl, 0x80 ; Drive number
-    int 0x13 ; BIOS disk services
+    int 0x13     ; BIOS disk services
     jc error_reading_disk
 
 ignore_disk_read_error:
@@ -23,45 +19,19 @@ error_reading_disk:
     cmp word [dap_sectors_num], READ_SECTORS_NUM
     jle ignore_disk_read_error
 
-    mov bx, error_reading_disk_msg
-    call print_string
-
 end:
     hlt
     jmp end
 
-;; Uses the BIOS to print a null-termianted string. The address of the
-;; string is found in the bx register.
-print_string:
-    pusha
-    mov ah, 0x0e ; BIOS "display character" function
-
-print_string_loop:
-    cmp byte [bx], 0
-    je print_string_return
-
-    mov al, [bx]
-    int 0x10 ; BIOS video services
-
-    inc bx
-    jmp print_string_loop
-
-print_string_return:
-    popa
-    ret
-
     align 4
 disk_address_packet:
-    db 0x10 ; Size of packet
-    db 0 ; Reserved, always 0
+    db 0x10                           ; Size of packet
+    db 0                              ; Reserved, always 0
 dap_sectors_num:
-    dw READ_SECTORS_NUM ; Number of sectors read
+    dw READ_SECTORS_NUM               ; Number of sectors read
     dd (BOOT_LOAD_ADDR + SECTOR_SIZE) ; Destination address
-    dq 1 ; Sector to start at (0 is the boot sector)
+    dq 1                              ; Sector to start at (0 is the boot sector)
 
 READ_SECTORS_NUM equ 64
-BOOT_LOAD_ADDR equ 0x7c00
-SECTOR_SIZE equ 512
-
-hello_msg: db "AIAOS Hello", 13, 10, 0
-error_reading_disk_msg: db "AIAOS ERROR: failed to read disk with 0x13/ah=0x42", 13, 10, 0
+BOOT_LOAD_ADDR   equ 0x7c00
+SECTOR_SIZE      equ 512
